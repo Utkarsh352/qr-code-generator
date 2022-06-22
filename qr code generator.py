9dd718cde9ddcf65d856b66481118022eeecce7e
Utkarsh Mahajan <89238212+Utkarsh352@users.noreply.github.com>
2022-06-23 04:27:03 +0530

    Update qr code generator.py

--- qr code generator.py
+++ qr code generator.py
@@ -1,6 +1,6 @@
 #pip install qrcode use this command to download the qrcode module
 import qrcode as qr
-link=input("enter link or text")
-name=input("what should be the name of the file")
+link=input("enter link\n")
+name=input("what should be the name of the file\n")
 image=qr.make(f"{link}")
 image.save(f"{name}.png")
